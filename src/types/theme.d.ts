1ff987a1391fe53b7765675dea1f520f19f08c18
Okposong Ebong <okposong.ebong@celus.io>
2024-12-19 23:22:34 +0100

    fix: detect default theme value correctly

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,12 +1,24 @@
 import { IAppContext } from './app'
 import { PAGES } from './pages'
 
-export type tColorScheme = 'light' | 'dark' | 'system'
+export type TColorScheme = 'light' | 'dark' | 'system'
 
-export interface iThemeState {
-  mode: tColorScheme
+export interface IThemeState {
+  mode: TColorScheme
 }
-export interface iThemeContextValue extends iThemeState {
-  onChange: (mode?: tColorScheme) => void
-  onToggle: ()=> void 
+export interface IThemeProps {
+  /**
+   * Mode should be a stateful property.
+   * It determines the state of the icon
+   */
+  mode?: TColorScheme
+  /**
+   * Function to handle the change in theme action
+   * @returns
+   */
+  onChange?: () => void | undefined
+}
+export interface IThemeContextValue extends IThemeState {
+  onChange: (mode?: TColorScheme) => void
+  onToggle: () => void
 }
